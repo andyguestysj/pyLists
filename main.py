c6cb392424459494038b2470bbfdc16567408b41
andyguest <59604680+andyguestysj@users.noreply.github.com>
2021-10-11 18:37:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+def main():
+
+  # Lists
+
+  first_list = [1,7,3,8,4,5,3,7,4,2,3]
+
+  # 1. Output the fourth item in first_list
+
+  # 2. Output the third, fourth and fifth items in first_list
+
+  # 3. Change the second item in first_list to 0 and print it out
+
+  # 4. Add 5 to the end of first_list and print it out
+
+  # 5. Remove 8 from first_list and print it out
+
+  # 6. Delete everything from first_list and print it out
+
+  first_list = [1,7,3,8,4,5,3,7,4,2,3]
+  second_list = [10,11,12]
+
+  # 7. Add second_list on to the end of first_list and print it out
+
+  # Dictionary
+
+  name_dict = {'Andy':'Guest','Mike':'ODea','Aminu':'Usman'}
+
+  # 1. Output name_dict
+
+  # 2. Output the keys for name_dict
+
+  # 3. Output the values for name_dict
+
+  # 4. Query name_dict for my surname and print it out
+
+  # 5. Add your name to name_dict in the same format
+  
+  print (name_dict['Andy'])
+
+
+if __name__ == "__main__":
+  main()
+
